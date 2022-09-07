81ec4bdc77dd9575ef3b994ee9f607878e2b4cb6
*Diogo <diogolimakas@hotmail.com>
2022-09-07 13:00:17 -0300

    adicionadas funções de dar play ir para proxima e voltar para anterior em JavaScript

--- js/player.js
+++ js/player.js
@@ -1,10 +1,68 @@
 export default class Player {
-  constructor(){
+  constructor(goPlay, goBack, goNext) {
+    this.goNext = document.querySelector(goNext)
+    this.goBack = document.querySelector(goBack)
+    this.goPlay = document.querySelector(goPlay);
+    this.i = 0; 
+    this.audio = new Audio();
 
+    this.audios = {
+      src: ["../../assets/audio/hanging.mp3", "../../assets/audio/Snacks.mp3", "../../assets/audio/Still.mp3"]
+    }
   }
-  init(){
+
+  // da o play na musica
+  playAudio() {
+    this.audios.src.forEach((item) => {
+      return item;
+    });
+    this.audio.src = this.audios.src[0];
+    this.audio.play();
+  }
+
+  // Passa para a próxima musica
+  nextAudio() {
+
+    // i é o valor inicial do index que é igual a zero
+    this.i += 1;
+
+    //Se passar o taol da de item da string volta do zero novamente
+    this.i = this.i % this.audios.src.length;
+
+    // Atribui o próximo index ao src atual 
+    this.audio.src = this.audios.src[this.i];
+
+    // Ativa o play da musica 
+    this.audio.play();
+  }
+
+  // Volta para musica anterior 
+  prevAudio() {
+    this.i === 0 ? this.i = this.audios.src.length : '';
+    this.i = this.i - 1;
+    this.audios.src[this.i];
+    this.audio.src = this.audios.src[this.i];
+    this.audio.play();
+  }
+
+  // Pega o evento de clique
+  activeEvent() {
+    this.goPlay.addEventListener('click', this.playAudio)
+    this.goNext.addEventListener('click', this.nextAudio)
+    this.goBack.addEventListener('click', this.prevAudio)
+  }
+
+  // Faz o bind dos eventos
+  bindEvents() {
+    this.playAudio = this.playAudio.bind(this);
+    this.nextAudio = this.nextAudio.bind(this);
+    this.prevAudio = this.prevAudio.bind(this);
+  }
+
+  // Inicia a Class
+  init() {
+    this.bindEvents();
+    this.activeEvent();
     return this;
   }
 };
-
-
--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import Player from './player.js'
 
-const player = new Player();
+const player = new Player('[data-play-js]', '[data-anterior-js]','[data-proxima-js]');
+
 player.init();
